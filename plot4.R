8fa4805f540238090bca9b4c0cb9309b5b8dfdd9
Ana4brief <anapascal.research@gmail.com>
2015-07-12 22:09:56 +0100

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -3,6 +3,7 @@ data=read.table("household_power_consumption.txt",header=T,sep=";",na.strings="?
 dates=as.Date(as.character(data$Date),"%d/%m/%Y")
 selected=data[dates>="2007-02-01" & dates<="2007-02-02",]
 
+selected$DateTime=strptime(paste(selected$Date, selected$Time, sep=" "), format = "%d/%m/%Y %H:%M:%S")
 par(mfrow=c(2,2))
 plot(selected$DateTime,as.numeric(selected$Global_active_power),type='l',xlab="",ylab="Global Active Power")
 plot(selected$DateTime,selected$Voltage,type='l',xlab="datetime",ylab="Voltage")
