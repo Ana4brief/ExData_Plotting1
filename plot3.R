a673d6e5e6cd323d50c7ea569f5e9d88911561ae
Ana4brief <anapascal.research@gmail.com>
2015-07-12 22:09:28 +0100

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -3,6 +3,7 @@ data=read.table("household_power_consumption.txt",header=T,sep=";",na.strings="?
 dates=as.Date(as.character(data$Date),"%d/%m/%Y")
 selected=data[dates>="2007-02-01" & dates<="2007-02-02",]
 
+selected$DateTime=strptime(paste(selected$Date, selected$Time, sep=" "), format = "%d/%m/%Y %H:%M:%S")
 plot(selected$DateTime,selected$Sub_metering_1,type='l',col="Black",xlab="",ylab="Energy sub metering")
 lines(selected$DateTime,selected$Sub_metering_2,col="Red")
 points(selected$DateTime,selected$Sub_metering_3,type='l',col="Blue")
